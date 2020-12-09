de9edc2b3fec04550bf4d74456f7168e7294223a
xiehuaqiang <mrxiehuaqiang@163.com>
2020-12-09 17:16:24 +0800

    升级路由

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -6,7 +6,32 @@
 -->
 
 <template>
-  <div>
-    3333
+  <div class="content">
+    <div class="son-cont">1</div>
+    <div class="son-cont">2</div>
+    <div class="son-cont">3</div>
+    <div class="son-cont">4</div>
+    <div class="son-cont">5</div>
+    <div class="son-cont">6</div>
   </div>
 </template>
+
+<style lang="scss">
+  .content {
+    position: fixed;
+    right: 0;
+    height: 768px;
+    display: flex;
+    background: pink;
+    flex-direction: column;
+    overflow: hidden;
+    overflow-y: scroll;
+  }
+  .son-cont {
+    flex-shrink: 0;
+    width: 400px;
+    height: 130px;
+    border-radius: 4px;
+    background: lightblue;
+  }
+</style>
