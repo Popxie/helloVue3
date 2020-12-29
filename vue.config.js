8a4a1293782e4f0160c666400390deda84177319
KaKa_Xie <mrxiehuaqiang@163.com>
2020-12-30 00:28:08 +0800

    修改配置文件信息

--- vue.config.js
+++ vue.config.js
@@ -71,7 +71,7 @@ module.exports = {
           enforce: true
         },
         elementUI: {
-          name: "chunk-elementui",
+          name: "chunk-element-plus",
           test: /[\\/]node_modules[\\/]element-plus[\\/]/,
           chunks: "all",
           priority: 3,
